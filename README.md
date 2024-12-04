f3011010fa9deac512c8f470b5912d34e3f3b26b
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-12-04 14:49:47 +0300

    Updated the readme file and made a directory

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-prac
 This is a space for all the exercises that i have undergone in my Python learning curve
+
+# Topics Covered
+- Here are the topics covered in this practical folder:
+## Intro
+## Variables
---
+++ prac-variables/var_type.py
@@ -0,0 +1,18 @@
+# x = "John"
+# y = 23
+
+# print(x + " is of age " + str(y))
+# print(x + " is of type " + str(type(x)))
+# print(str(y) + " is of type " + str(type(y)))
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x, y, z)
+
+x = "fun"
+
+def myfunc():
+    y = "Python is"
+    print(y + " " + x)
+
+myfunc()
